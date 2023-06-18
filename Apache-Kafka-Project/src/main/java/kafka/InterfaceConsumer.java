a9655750f41abc465ee14c6882b873bec47efd07
fiorotticaio <caiofiorotti@gmail.com>
2023-06-18 12:15:32 -0300

    :sparkles: changing to kafka stream

--- Apache-Kafka-Project/src/main/java/kafka/InterfaceConsumer.java
+++ Apache-Kafka-Project/src/main/java/kafka/InterfaceConsumer.java
@@ -1,20 +1,15 @@
 package kafka;
 
-import java.util.Arrays;
 import java.util.Properties;
-import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 
 
 public class InterfaceConsumer {
@@ -23,82 +18,51 @@ public class InterfaceConsumer {
         /* Kafka configuration */
         String BootstrapServer = "localhost:9092"; 
         String sourceTopic = "coffee_sales"; 
-        String destinationTopic = "coffee_price";
-        int web_coffee_price = 1;
+        String destinationTopic = "web_coffee_price";
 
-        /* Creating consumer to receive the raw coffee buy event on interface */
-        /* Consumer settings */
-        Properties propConsumer = new Properties();
-        propConsumer.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServer);
-        propConsumer.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        propConsumer.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        propConsumer.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        /* Creating kafka stream props */
+        Properties kStreamProps = new Properties();
+        kStreamProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "coffee-price-interface-processor");
+        kStreamProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServer);
 
-        /* Consumer group settings */
-        propConsumer.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "coffee-sales-consumer-group");
-        propConsumer.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        propConsumer.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        propConsumer.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1000");
 
-        /* Creating consumer to receive the records from UI calls */
-        Consumer<String, String> consumer = new KafkaConsumer<>(propConsumer); 
-        consumer.subscribe(Arrays.asList(sourceTopic)); 
+        /* Creating stream builder */
+        StreamsBuilder builder = new StreamsBuilder();
+        
 
-        /* Creating producer to send coffee value got from interface to the topic */
-        /* Setting new producer properties */
-        Properties propProducer = new Properties();
-        propProducer.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BootstrapServer);
-        propProducer.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        propProducer.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        /* Creating kStream to recive the data from source topic */
+        KStream<String, String> sourceStream = builder.stream(sourceTopic,
+            Consumed.with(Serdes.String(), Serdes.String()));
 
-        /* Instantiating new producer */
-        KafkaProducer<String, String> producer = new KafkaProducer<>(propProducer);
 
-        /* Initialization of the parameter that controls the amount of sales registered */
-        int countCoffeeSales = 0;
-        Double coffeeValue=0.0;
-        int inflationFactor=0;
-
-        /* Loop to consume messages */
-        while (true) {
-            /* Polling from web partition, each second */
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000)); 
+        AtomicInteger countCoffeeSales = new AtomicInteger(0); // Initializing the counter
             
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Received message: " + record.value());
-                coffeeValue = Double.parseDouble(record.value()); 
-                countCoffeeSales++;
-                inflationFactor=0;
-
-                System.out.println("COUNT: " + countCoffeeSales);
+        /* Creating Ktream to recive the web price event */
+        KStream<String, String> webPriceValuesStream = sourceStream
+            .mapValues(value -> {
+                System.out.println("Received message: " + value);
+                Double coffeeValue = Double.parseDouble(value); 
                 
-            }
+                System.out.println("COUNT: " + countCoffeeSales.incrementAndGet()); // Incrementing the counter
+        
+                /* If sales surpass some threshold, the overall price of coffee rises */
+                if (countCoffeeSales.get() >= 5) { 
+                    coffeeValue *= 2;    
+                    System.out.println("Aumentou o preco do cafeh: " + coffeeValue);
+                    countCoffeeSales.set(0); // Resetting the counter
 
-            /* After 10 seconds of idle on interface, prices starts to decay */
-            if (inflationFactor++ >= 10) {
-                coffeeValue*=0.9;
-                if (coffeeValue<0.0) coffeeValue=0.0;
-                sendSalesToTopic(destinationTopic, web_coffee_price, BootstrapServer, coffeeValue, producer);
-            }
+                    return coffeeValue.toString();
+                } else {
+                    return "0.0";
+                }
+            });
 
-            /* If sales surpass some threshold, the overall price of coffee rises */
-            if (countCoffeeSales >= 5) { 
-                Double newCoffeeValue = coffeeValue*1.5;    
-                sendSalesToTopic(destinationTopic, web_coffee_price, BootstrapServer, newCoffeeValue, producer);
-                System.out.println("Aumentou o preço do café");
-                countCoffeeSales = 0; 
-            }
-        }
-    }
+        /* Sending the web price to the destination topic */
+        webPriceValuesStream.to(destinationTopic, Produced.with(Serdes.String(), Serdes.String()));
 
 
-    private static void sendSalesToTopic(String topic, int partition, String BootstrapServer, 
-        Double newCoffeeValue, KafkaProducer<String, String> producer){
-
-        /* Creating a record on the partition, with the new coffee value */
-        String coffeeValueStr = Double.toString(newCoffeeValue);
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, partition, "UI_change", coffeeValueStr);
-        producer.send(record);
-    }
+        /* Creating kafka stream */
+        KafkaStreams streams = new KafkaStreams(builder.build(), kStreamProps);
+        streams.start();
+    } 
 }
-
